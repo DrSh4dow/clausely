194b8a8b49ab3ad327b42a07745d29c2ab72bdd8
drsh4dow <daniel.morettiv@gmail.com>
2024-04-20 13:11:17 -0400

    added a warning popover

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { ArrowDownIcon, MenuIcon } from 'lucide-svelte';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import * as Popover from '$lib/components/ui/popover';
 </script>
 
 <header class="px-8 pt-8 sm:pt-12 md:pt-14 lg:pt-16">
@@ -12,12 +13,17 @@
 			<a href="/" class="font-recoleta text-[32px] font-black">Clausely</a>
 		</div>
 		<div class="hidden sm:block">
-			<Button variant="outlinePrimary">
-				<div class="flex items-center justify-center gap-x-1">
-					<ArrowDownIcon class="h-6 w-6 min-w-6" />
-					<h5>Clausely extension</h5>
-				</div>
-			</Button>
+			<Popover.Root>
+				<Popover.Trigger>
+					<Button variant="outlinePrimary">
+						<div class="flex items-center justify-center gap-x-1">
+							<ArrowDownIcon class="h-6 w-6 min-w-6" />
+							<h5>Clausely extension</h5>
+						</div>
+					</Button>
+				</Popover.Trigger>
+				<Popover.Content>Work in progress</Popover.Content>
+			</Popover.Root>
 		</div>
 		<div class="sm:hidden">
 			<DropdownMenu.Root>
