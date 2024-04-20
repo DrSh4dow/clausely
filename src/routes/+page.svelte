328e803d1db6b5264d843c629a6f2c8fce6781ce
drsh4dow <daniel.morettiv@gmail.com>
2024-04-19 21:09:06 -0400

    added new components & colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1 class="text-xl font-semibold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-xl font-semibold">Clausely</h1>
+
+<p class="font-recoleta">This text will use deafult recoleta</p>
